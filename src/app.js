78431b23db1b40b8d278612f6895a1c81fea4d0f
Whitney Harvey <wmelrose01@gmail.com>
2016-03-14 15:50:16 -0700

    add webpack

---
+++ src/app.js
@@ -0,0 +1,30 @@
+const angular = require('angular');
+const app = require('./js/app-module');
+
+const template = require('./template.html');
+const homeTemplate = require('./home.html');
+
+import './css/main.css';
+
+app.config( function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise('/home');
+
+  $stateProvider
+    .state( 'home', {
+      url: '/images',
+      template: homeTemplate,
+      resolve: {
+        images ( ImageService ) {
+          return ImageService.query().$promise;
+        }
+      },
+      controller: function( $scope, images ) {
+        $scope.images = images;
+      }
+    });
+});
+
+document.body.innerHTML = template;
+
+angular.bootstrap( document, [ app.name ], {} );
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
+const path = require( 'path' );
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve( __dirname, '/dist/public' ),
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "full-stack test"
+    }),
+  module: {
+    preLoaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules|lib/,
+                loader: "jshint-loader"
+            }
+        ],
+    loaders: [
+      { test: /\.css$/,loader: 'style!css'},
+      { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' },
+      {
+        test: /\.html$/,
+        loader: 'html'
+      }
+    ]
+  },
+  jshint: {
+    esversion: 6
+  }
+};
